5091f6de0e9b860fd3e768feacc46bdbb27e732e
Frank Müller <101019006+fmabap@users.noreply.github.com>
2024-09-28 11:45:52 +0200

    single file vite plugin

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,69 @@
-import { countGoalColors, countColors, initGoal, goal, countRounds } from "./game";
-import { addClickEventToPins, addClickEventToUndo, addClickEventToCheck, setGoal, showGameRounds, startNextRound, addClickEventToNewGame, addClickEventToHeader, handleKeyboard } from "./ui";
-let goal: goal;
-window.addEventListener("load", () => { init() });
+import {
+  countGoalColors,
+  countColors,
+  initGoal,
+  goal,
+  countRounds,
+} from "./game";
+import {
+  addClickEventToPins,
+  addClickEventToUndo,
+  addClickEventToCheck,
+  setGoal,
+  showGameRounds,
+  startNextRound,
+  addClickEventToNewGame,
+  addClickEventToHeader,
+  handleKeyboard,
+} from "./ui";
+let theGoal: goal;
+window.addEventListener("load", () => {
+  init();
+});
 /**
  * Init Mastermind
  */
 function init() {
-    addClickEventToPins();
-    addClickEventToUndo();
-    addClickEventToCheck();
-    addClickEventToNewGame();
-    addClickEventToHeader();
-    handleKeyboard();
-    showGameRounds(countRounds);
-    goal = initGoal(countGoalColors, countColors);
-    setGoal(goal);
-    startNextRound();
-    setText();
+  addClickEventToPins();
+  addClickEventToUndo();
+  addClickEventToCheck();
+  addClickEventToNewGame();
+  addClickEventToHeader();
+  handleKeyboard();
+  showGameRounds(countRounds);
+  theGoal = initGoal(countGoalColors, countColors);
+  setGoal(theGoal);
+  startNextRound();
+  setText();
 }
 
 function setText() {
-    const divUndo = <HTMLDivElement>document.getElementById("undo");
-    const divCheck = <HTMLDivElement>document.getElementById("check");
-    const divNewGame = <HTMLDivElement>document.getElementById("newGame");
-    const divWon = <HTMLDivElement>document.getElementById("won");
-    const divLost = <HTMLDivElement>document.getElementById("lost");
-    // Check Browser Language
-    if (navigator.language.indexOf("de") > -1) {
-        if (window.innerWidth >= 640) {
-            divUndo.innerText = "Rückgängig";
-        }
-        else {
-            divUndo.innerText = "Rück-gängig";
-        }
-
-        divCheck.innerText = "Prüfen";
-        divNewGame.innerText = "Neues Spiel";
-        divWon.innerText = "Du hast gewonnen";
-        divLost.innerText = "Du hast verloren";
-    }
-    else {
-        divUndo.innerText = "Undo";
-        divCheck.innerText = "Check";
-        divNewGame.innerText = "New Game";
-        divWon.innerText = "You won";
-        divLost.innerText = "You lost";
+  const divUndo = <HTMLDivElement>document.getElementById("undo");
+  const divCheck = <HTMLDivElement>document.getElementById("check");
+  const divNewGame = <HTMLDivElement>document.getElementById("newGame");
+  const divWon = <HTMLDivElement>document.getElementById("won");
+  const divLost = <HTMLDivElement>document.getElementById("lost");
+  // Check Browser Language
+  if (navigator.language.indexOf("de") > -1) {
+    if (window.innerWidth >= 640) {
+      divUndo.innerText = "Rückgängig";
+    } else {
+      divUndo.innerText = "Rück-gängig";
     }
 
+    divCheck.innerText = "Prüfen";
+    divNewGame.innerText = "Neues Spiel";
+    divWon.innerText = "Du hast gewonnen";
+    divLost.innerText = "Du hast verloren";
+  } else {
+    divUndo.innerText = "Undo";
+    divCheck.innerText = "Check";
+    divNewGame.innerText = "New Game";
+    divWon.innerText = "You won";
+    divLost.innerText = "You lost";
+  }
 }
 
 export function getGoal(): goal {
-    return goal;
+  return theGoal;
 }
-
-
-
-
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // vite.config.js
 export default defineConfig({
     // config options
+    plugins: [
+        viteSingleFile()],
     base: "./",
     build: {
         outDir: "./docs",
