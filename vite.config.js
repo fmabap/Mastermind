9395610d8b618ca381bd6b27949f8c9ad8c3f6c4
Frank Müller <101019006+fmabap@users.noreply.github.com>
2024-09-28 12:00:29 +0200

    make pwa app

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 // vite.config.js
 export default defineConfig({
     // config options
-    plugins: [
+    plugins: [VitePWA(
+        {
+            registerType: "autoUpdate",
+            manifest: {
+                name: "MasterMind",
+                short_name: "MasterMind",
+                theme_color: "#000000",
+                icons: [
+                    {
+                        src: "./icon.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable"
+                    }
+                ]
+            }
+        }),
         viteSingleFile()],
     base: "./",
     build: {
